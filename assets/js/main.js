b59fd8e9af9bd1a2fa4e6d64b9894785d7554559
Aleksandr <a.kuptcov.90@gmail.com>
2023-12-03 22:13:22 +0500

    add products tabs js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@
 const swiperCategories = new Swiper(".categories__container", {
   spaceBetween: 24,
   loop: true,
-  
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -37,3 +37,23 @@ const swiperCategories = new Swiper(".categories__container", {
 /*=============== SWIPER PRODUCTS ===============*/
 
 /*=============== PRODUCTS TABS ===============*/
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[content]");
+
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+      const target = document.querySelector(tab.dataset.target);
+      tabContents.forEach((tabContents) => {
+        tabContents.classList.remove('active-tab');
+      });
+
+      target.classList.add('active-tab');
+
+      tabs.forEach((tab) => {
+        tab.classList.remove('active-tab');
+      });
+
+      tab.classList.add('active-tab');
+    });
+
+  });
